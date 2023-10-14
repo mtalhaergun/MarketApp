211ec44e46715289f2e31b5a9fd0b1704de7ee92
Talha <mtalhaergun24@gmail.com>
2023-10-14 22:13:08 +0300

    Added GridSpacingItemDecoration for recycler view

--- app/src/main/java/com/quenhwyfar/marketapp/ui/products/ProductsFragment.kt
+++ app/src/main/java/com/quenhwyfar/marketapp/ui/products/ProductsFragment.kt
@@ -1,5 +1,6 @@
 package com.quenhwyfar.marketapp.ui.products
 
+import android.graphics.Rect
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,6 +10,7 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
 import com.quenhwyfar.marketapp.R
 import com.quenhwyfar.marketapp.databinding.FragmentProductsBinding
 import com.quenhwyfar.marketapp.domain.uimodel.Products
@@ -60,6 +62,7 @@ class ProductsFragment : Fragment() {
 
         })
         binding.rvProducts.adapter = productAdapter
+        binding.rvProducts.addItemDecoration(GridSpacingItemDecoration(3, spacing = 24))
         productAdapter.getDao(viewModel.getProductsDao())
     }
 
@@ -85,6 +88,20 @@ class ProductsFragment : Fragment() {
         binding.cart.setOnClickListener {
             val navigation = ProductsFragmentDirections.actionProductsFragment2ToCartFragment()
             Navigation.findNavController(it).navigate(navigation)
+        }
+    }
+
+    inner class GridSpacingItemDecoration(private val spanCount: Int, private val spacing: Int) : RecyclerView.ItemDecoration() {
+        override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+            val position = parent.getChildAdapterPosition(view)
+            val column = position % spanCount
+
+            outRect.left = spacing - column * spacing / spanCount
+            outRect.right = (column + 1) * spacing / spanCount
+
+            if (position >= spanCount) {
+                outRect.top = spacing
+            }
         }
     }
 
